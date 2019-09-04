f1c6e366ff0566a74aeda7c5a41e65e7bc43bbd5
PetrDvoracek <pedro.dvoracek@gmail.com>
2019-09-04 19:09:35 +0200

    antd Login and SignUp GUI, added antd to App.css

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, { useCallback, useContext } from "react";
+import "./Login.css";
 import { withRouter, Redirect } from "react-router";
 import app from "../../config/firebase";
 import { AuthContext } from "./AuthContext";
+import { Typography, Form, Icon, Input, Button, Checkbox } from "antd";
+const { Title, Text } = Typography;
 
 const Login = ({ history }) => {
   const handleLogin = useCallback(
@@ -27,20 +30,37 @@ const Login = ({ history }) => {
   }
 
   return (
-    <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleLogin}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Log in</button>
-      </form>
-    </div>
+    <Form onSubmit={handleLogin} className="login-form">
+      <div className="login-form-title">
+        <Title>EquipPex</Title>
+        <Text disabled>There is always better solution.</Text>
+      </div>
+      <Form.Item>
+        <Input
+          prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+          placeholder="Username"
+          name="email"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Input
+          prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+          type="password"
+          placeholder="Password"
+          name="password"
+        />
+      </Form.Item>
+      <Form.Item>
+        <Checkbox>Remember me</Checkbox>
+        <a className="login-form-forgot" href="/forgot-password">
+          Forgot password
+        </a>
+        <Button type="primary" htmlType="submit" className="login-form-button">
+          Log in
+        </Button>
+        Or <a href="/sign-up">register now!</a>
+      </Form.Item>
+    </Form>
   );
 };
 
--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,40 +1,112 @@
-import React, { useCallback } from "react";
-import { withRouter } from "react-router";
+import React, { useCallback, useContext, useState } from "react";
+import { withRouter, Redirect } from "react-router";
 import app from "../../config/firebase";
+import { Typography, Form, Icon, Input, Button } from "antd";
+import { AuthContext } from "./AuthContext";
+import "./Login.css";
 
-const SignUp = ({ history }) => {
-  const handleSignUp = useCallback(
+const { Title, Text } = Typography;
+
+const SignUpBase = props => {
+  const [confirmDirty, setConfirmDirty] = useState(false);
+  const handleSubmit = useCallback(
     async event => {
       event.preventDefault();
-      const { email, password } = event.target.elements;
+      const { email, passwordFirst, passwordSecond } = event.target.elements;
+      if (passwordFirst.value !== passwordSecond.value) {
+        alert("passwords are not same!");
+        return;
+      }
       try {
         await app
           .auth()
-          .createUserWithEmailAndPassword(email.value, password.value);
-        history.push("/");
+          .createUserWithEmailAndPassword(email.value, passwordFirst.value);
+        props.history.push("/");
       } catch (error) {
         alert(error);
       }
     },
-    [history]
+    [props.history]
   );
 
+  const validateToNextPassword = (rule, value, callback) => {
+    const { form } = props;
+    if (value && confirmDirty) {
+      form.validateFields(["confirm"], { force: true });
+    }
+    callback();
+  };
+  const compareToFirstPassword = (rule, value, callback) => {
+    const { form } = props;
+    if (value && value !== form.getFieldValue("password")) {
+      callback("Two passwords that you enter is inconsistent!");
+    } else {
+      callback();
+    }
+  };
+
+  const handleConfirmBlur = e => {
+    const { value } = e.target;
+    setConfirmDirty({ confirmDirty: confirmDirty || !!value });
+  };
+  const { currentUser } = useContext(AuthContext);
+  if (currentUser) {
+    return <Redirect to="/" />;
+  }
+  const { getFieldDecorator } = props.form;
+
   return (
-    <div>
-      <h1>Sign up</h1>
-      <form onSubmit={handleSignUp}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+    <Form className="login-form" onSubmit={handleSubmit}>
+      <Form.Item label="E-mail">
+        {getFieldDecorator("email", {
+          rules: [
+            {
+              type: "email",
+              message: "The input is not valid E-mail!"
+            },
+            {
+              required: true,
+              message: "Please input your E-mail!"
+            }
+          ]
+        })(<Input />)}
+      </Form.Item>
+      <Form.Item label="Password" hasFeedback>
+        {getFieldDecorator("password", {
+          rules: [
+            {
+              required: true,
+              message: "Please input your password!"
+            },
+            {
+              validator: validateToNextPassword
+            }
+          ]
+        })(<Input.Password />)}
+      </Form.Item>
+      <Form.Item label="Confirm Password" hasFeedback>
+        {getFieldDecorator("confirm", {
+          rules: [
+            {
+              required: true,
+              message: "Please confirm your password!"
+            },
+            {
+              validator: compareToFirstPassword
+            }
+          ]
+        })(<Input.Password onBlur={handleConfirmBlur} />)}
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Register
+        </Button>
+      </Form.Item>
+    </Form>
   );
 };
+
+const SignUp = Form.create({ name: "register" })(SignUpBase);
 
 export default withRouter(SignUp);
